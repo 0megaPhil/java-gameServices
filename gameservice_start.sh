ef25b547726ad679941b6dad9e3545680b00f58e
Phil Platt <pplatt@firmys.com>
2022-04-21 13:48:44 -0700

    Debug issues with deploy

--- gameservice_start.sh
+++ gameservice_start.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 # Expects environment to be added
 # TODO - Setup proper logging via logback
-pkill -ef 'java -jar' & pkill -ef 'java -jar' & pkill -ef 'java -jar'
-sleep 15 # FIXME with proper checking that old process is dead and port is clear
+pkill -ef 'java -jar'
+sleep 5 # FIXME with proper checking that old process is dead and port is clear
+pkill -ef 'java -jar'
+sleep 5 # FIXME with proper checking that old process is dead and port is clear
+pkill -ef 'java -jar'
+sleep 10 # FIXME with proper checking that old process is dead and port is clear
 source sshenv
 export SERVICE_COMMAND="java -jar ~/$(ls *gameservices*.jar) --server.port=8080 \
 --spring.datasource.url=$SPRING_DATASOURCE_URL --spring.datasource.username=$SPRING_DATASOURCE_USERNAME \
