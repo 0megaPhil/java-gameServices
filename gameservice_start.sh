47420f8fb4ec251ddc000d842f1b6de77de1935d
Phil Platt <pplatt@firmys.com>
2022-04-21 14:03:38 -0700

    Try service command twice if fails

--- gameservice_start.sh
+++ gameservice_start.sh
@@ -3,10 +3,10 @@
 # TODO - Setup proper logging via logback
 pkill -ef 'java -jar'
 sleep 5 # FIXME with proper checking that old process is dead and port is clear
-fuser -k "$SERVICE_PORT/tcp"
+fuser -k $SERVICE_PORT/tcp
 sleep 10 # FIXME with proper checking that old process is dead and port is clear
 source sshenv
 export SERVICE_COMMAND="java -jar ~/$(ls *gameservices*.jar) --server.port=$SERVICE_PORT \
 --spring.datasource.url=$SPRING_DATASOURCE_URL --spring.datasource.username=$SPRING_DATASOURCE_USERNAME \
 --spring.datasource.password=$SPRING_DATASOURCE_SECRET"
-nohup bash -c "$SERVICE_COMMAND" > gameservices.log 2>&1 & exit &
+nohup bash -c "$SERVICE_COMMAND || $SERVICE_COMMAND" > gameservices.log 2>&1 & exit &
